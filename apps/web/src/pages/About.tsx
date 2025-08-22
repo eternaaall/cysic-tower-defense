d06b12206499fd196ce319e61b918325093f30f0
eternaaall <156612897+eternaaall@users.noreply.github.com>
2025-08-22 18:55:38 +0200

    Update About.tsx

--- apps/web/src/pages/About.tsx
+++ apps/web/src/pages/About.tsx
@@ -8,7 +8,7 @@ export default function About(){
 
       <div className="about-card">
         <p><strong>Author:</strong> eternaaall — a simple 18-year-old ZK fan from Ukraine. I work at a factory and enjoy crypto. Look for <b>eternaaall</b> on all social networks.</p>
-        <p><strong>My Linktr:</strong> <a href="https://linktr.ee/eternaaall" target="_blank" rel="noreferrer">https://linktr.ee/eternaaall</a></p>
+        <p><strong>My Linktr:</strong> <a href="https://linktr.ee/eternaaall" target="_blank" rel="noreferrer">Linktr</a></p>
         <p><strong>Discord:</strong> eternaaall</p>
         <p><strong>My scripts for easy node installation:</strong> <a href="https://github.com/eternaaall/provercysic" target="_blank" rel="noreferrer">Prover</a> / <a href="https://github.com/eternaaall/verifcysic" target="_blank" rel="noreferrer">Verifier</a></p>
         <p><strong>Cysic:</strong> <a href="https://app.cysic.xyz" target="_blank" rel="noreferrer">app.cysic.xyz</a></p>
